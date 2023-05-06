7720f7ba00f1b3e88fba9e6dc17a550441c9b2df
Beau VonGunten <bjvong@gmail.com>
2023-05-06 17:16:20 -0400

    the next level seven scope

--- src/config/google.js
+++ src/config/google.js
@@ -7,6 +7,7 @@ passport.use(new GoogleStrategy(
         clientID: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID,
         clientSecret: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_SECRET,
         callbackURL: 'https://gentle-ocean-27020.herokuapp.com/auth/google/callback',
+        scope: ['email']
     },
     async (accessToken, refreshToken, profile, done) => {
         console.log('user profile is:', profile);
